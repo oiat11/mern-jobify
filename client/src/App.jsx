11f6c5fb2d3a5567fa81a871cb3d4b8c54bc327f
oiat11 <fu.qia@northeastern.edu>
2024-10-21 12:39:55 -0700

    set up the react router for home page and about page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'; 
+
+const router = createBrowserRouter([
+  {
+  path:'/',
+  element: <h1>home</h1>
+  
+},
+{
+  path:'/about',
+  element: (<div><h1>about page</h1></div>)
+  
+},
+]) 
 
 function App() {
   return (
-    <h1>Jobify App</h1>
-  )
+    <RouterProvider router={router} />  
+  );
 }
 
 export default App
