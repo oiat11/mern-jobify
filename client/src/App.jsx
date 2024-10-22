31688f5fd9c2ff6779646dd2db0e347165b9d2e5
oiat11 <fu.qia@northeastern.edu>
2024-10-22 11:17:56 -0700

    set the child route in App.jsx and added Outlet to homeLayout.jsx to render the child route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,38 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'; 
-import { DashboardLayout, HomeLayout, Register, Login } from './pages'
+import { DashboardLayout, HomeLayout, Register, Login, Landing } from './pages'
 
+// using an array to pass the structure to createBrowserRouter 
 const router = createBrowserRouter([
   {
   path:'/',
-  element: <HomeLayout/>
+  element: <HomeLayout/>,
+  children: [
+    {
+      // when index: trur is set, this represents the default route when visiting the parent path
+      index:true,
+      element: <Landing/>
+    },
+    {
+      path:'register',
+      element: <Register/>
+      
+    },
+    {
+      path:'login',
+      element: <Login/>
+      
+    },
+    {
+      path:'dashboard',
+      element: <DashboardLayout/>
+      
+    },
+
+  ]
   
 },
-{
-  path:'/register',
-  element: <Register/>
-  
-},
-{
-  path:'/login',
-  element: <Login/>
-  
-},
-{
-  path:'/dashboard',
-  element: <DashboardLayout/>
-  
-},
+
 ]) 
 
 function App() {
--- client/src/pages/HomeLayout.jsx
+++ client/src/pages/HomeLayout.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const HomeLayout = () => {
   return (
     <div>
-      <h1>HomeLayout</h1>
+        <nav>navbar</nav>
+        {/* Outlet is a placeholder of child route that will be rendered*/}
+        <Outlet/>
     </div>
   )
 }
