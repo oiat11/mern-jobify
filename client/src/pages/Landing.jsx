da61ef7afa996b1e4ad8a98f333aa24b23871a4d
oiat11 <fu.qia@northeastern.edu>
2024-10-29 19:58:50 -0700

    fixed the typo

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -13,10 +13,10 @@ const Landing = () => {
       </nav>
       <div className='container page'>
         <div className='info'>
-          <h1>job <span>Tracking</span> App</h1>
+          <h1>job <span>Tracking</span> app</h1>
           <p>I'm baby air plant photo booth gatekeep, tumeric kitsch PBR&B flannel lyft. Master cleanse street art air plant DIY lumbersexual salvia everyday carry organic. Fingerstache slow-carb tousled sriracha hashtag farm-to-table unicorn, try-hard roof party hot chicken. Listicle messenger bag succulents hammock DSA, man bun small batch palo santo wayfarers roof party 90's kitsch hell of poke bitters. Celiac cliche kinfolk, man bun fam pabst neutral milk hotel plaid tote bag biodiesel +1 narwhal sustainable air plant. Adaptogen echo park pabst succulents bruh retro selvage YOLO umami waistcoat before they sold out. Roof party Brooklyn cupping food truck lo-fi squid pabst glossier same banjo irony tote bag wolf raw denim next level.
           </p>
-          <Link to="./register" className='btn register-lnk'>Register</Link>
+          <Link to="./register" className='btn register-link'>Register</Link>
           <Link to="./login" className='btn '>Login / Demo User</Link>
 
         </div>
