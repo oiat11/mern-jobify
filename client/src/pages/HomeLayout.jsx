20d2a2889903ed5df7484b39954c3fce3317d8aa
oiat11 <fu.qia@northeastern.edu>
2024-10-29 20:01:40 -0700

    deleted nav in HomeLayout.js

--- client/src/pages/HomeLayout.jsx
+++ client/src/pages/HomeLayout.jsx
@@ -4,7 +4,6 @@ import { Outlet } from 'react-router-dom'
 const HomeLayout = () => {
   return (
     <div>
-        <nav>navbar</nav>
         {/* Outlet is a placeholder of child route that will be rendered*/}
         <Outlet/>
     </div>
